5261f81248302aa3f2fddec8a9ef6b514807ea57
Darkrulerz <dark_rulerz@hotmail.com>
2011-06-06 12:50:07 +0200

    Spastische ombouwing voor het uitvinden van wie een project is en welke je lid van bent EN editProject werkt
    Signed-off-by: Darkrulerz <dark_rulerz@hotmail.com>

--- classes/class.user.php
+++ classes/class.user.php
@@ -12,7 +12,7 @@
  */
 class user {
     var $db, $id, $firstname, $insertion, $lastname, $password, $email, $year, $admin;
-    var $exists;
+    var $exists = false;
     
     function __construct($db, $id, $password = "") {
         $this->db = $db;
@@ -36,18 +36,38 @@ class user {
             $this->year = $fields['year'];
             $this->admin = $fields['admin'];
             $this->exists = true;
-        } else {
-            $this->exists = false;
         }
     }
 
     function getProjects() {
-        return $this->db->doQuery("
-            SELECT `projects`.name as `name`
-            FROM `projects`, `studenten`
-            WHERE `projects`.llnr = `studenten`.id
-            AND `studenten`.id = '" . $this->id . "';
+        $projects = array();
+        $i = 0;
+        $result = $this->db->doQuery("
+            SELECT `projects`.name as `name` FROM `teamleden`, `teams`, `projects`
+            WHERE `teamleden`.llnr = '" . $this->id . "'
+            AND `teams`.teamnr = `teamleden`.teamnr
+            AND `projects`.projectid = `teams`.projectid;
         ");
+        $result2 = $this->db->doQuery("
+            SELECT `name` FROM `projects` WHERE `llnr` = '" . $this->id . "';
+        ");
+        if ($result != false) {
+            while ($fields = mysql_fetch_assoc($result)) {
+                if (!in_array($fields['name'], $projects)) {
+                    $projects[$i] = $fields['name'];
+                    $i++;
+                }
+            }
+        }
+        if ($result2 != false) {
+            while ($fields = mysql_fetch_assoc($result2)) {
+                if (!in_array($fields['name'], $projects)) {
+                    $projects[$i] = $fields['name'];
+                    $i++;
+                }
+            }
+        }
+        return $projects;
     }
 
     function getTeams() {
